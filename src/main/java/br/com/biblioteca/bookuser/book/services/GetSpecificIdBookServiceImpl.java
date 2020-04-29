aba9f434c3ac5ee89e4dbabee5f8cd075c402767
Waldir Marques <waldir.marques@dcx.ufpb.br>
2020-04-29 00:32:39 -0300

    implementation of verify status book

--- src/main/java/br/com/biblioteca/bookuser/book/services/GetSpecificIdBookServiceImpl.java
+++ src/main/java/br/com/biblioteca/bookuser/book/services/GetSpecificIdBookServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.biblioteca.bookuser.book.services;
 
 import br.com.biblioteca.bookuser.book.Book;
 import br.com.biblioteca.bookuser.book.BookRepository;
+import br.com.biblioteca.bookuser.exceptions.BookNotAvailableException;
 import br.com.biblioteca.bookuser.exceptions.BookNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,10 @@ public class GetSpecificIdBookServiceImpl implements GetSpecificIdBookService {
 
     @Override
     public Book findBySpecificID(String specificId) {
-        return bookRepository.findBySpecificID(specificId).orElseThrow(BookNotFoundException::new);
+        Book book = bookRepository.findBySpecificID(specificId).orElseThrow(BookNotFoundException::new);
+        if (book.isStatus()){
+            throw new BookNotAvailableException();
+        }
+        return book;
     }
 }
